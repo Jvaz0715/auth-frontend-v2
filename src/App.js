1e8f0397b729a2493751164cb49b6f141847af78
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-31 16:35:36 -0400

    refactors router into own file MainRouter.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'; // gives us the ability to navigate multi pages
 import { ToastContainer } from 'react-toastify'; //put here to be available to everything
-
-import Nav from './components/Nav/Nav';
-import Home from './components/Home/Home';
-import Signup from './components/Signup/Signup';
-import Login from './components/Login/Login';
+import MainRouter from './MainRouter';
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Router>
+    <>
       <ToastContainer />
-       <Nav /> {/*We put this outside as it will be on all pages */}
-      <>
-        <Route exact path="/" component={Home} />
-        <Route  exact path="/sign-up" component={Signup} />
-        <Route  exact path="/login" component={Login} />
-      </>
-    </Router>
+      <MainRouter />
+    </>
   )
 }
 
---
+++ src/MainRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'; // gives us the ability to navigate multi pages
+
+import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+
+const MainRouter = () => {
+   return(
+      <Router>
+         <Nav /> {/*We put this outside as it will be on all pages */}
+         <>
+            <Route exact path="/" component={Home} />
+            <Route  exact path="/sign-up" component={Signup} />
+            <Route  exact path="/login" component={Login} />
+         </>
+      </Router>
+   )
+};
+
+export default MainRouter;
